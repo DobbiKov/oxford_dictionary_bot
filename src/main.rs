2586b903ec8ae3a71e060d15a02baace6d733e56
Yehor Korotenko <dobbikov@gmail.com>
2025-04-27 18:17:05 +0200

    made it work

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use oxford_dictionary_lib::search_dictionary;
+use oxford_dictionary_lib::{search_dictionary, ParseLinkResult};
 use teloxide::{
     adaptors::DefaultParseMode, dispatching::dialogue::GetChatId, prelude::*, types::User,
     RequestError,
@@ -33,7 +33,8 @@ async fn main() {
 }
 
 async fn start_command_handler(bot: teloxide::Bot, msg: Message) -> ResponseResult<()> {
-    let msg_to_send = "";
+    let msg_to_send =
+        "Welcome to the Oxford Dictionary Bot!\n Write me any word to learn it's meaning!";
 
     bot.send_message(msg.chat_id().unwrap(), msg_to_send).await;
     Ok(())
@@ -43,25 +44,23 @@ async fn usual_text_handler(bot: teloxide::Bot, msg: Message) -> ResponseResult<
     let msg_txt = msg.text();
     if let Some(txt) = msg_txt {
         let word = txt.to_owned();
-        match search_dictionary(&word).await {
+        match search_dictionary_wrapper(&word).await {
             Ok(search_res) => match search_res {
                 oxford_dictionary_lib::ParseLinkResult::ResultList(vec_r) => {
                     let mut txt_to_send =
-                        "This word is not found, but there are possible variants:".to_string();
+                        "This word is not found, but there are possible variants:\n".to_string();
                     vec_r.iter().for_each(|el| {
-                        let mut temp_str = el.to_owned();
-                        temp_str.push_str("\\n");
+                        let mut temp_str = format!("{}\n", el);
                         txt_to_send.push_str(&temp_str);
                     });
 
                     bot.send_message(msg.chat_id().unwrap(), txt_to_send).await;
                 }
                 oxford_dictionary_lib::ParseLinkResult::MeaningsList(vec_r) => {
-                    let mut txt_to_send =
-                        "This word is not found, but there are possible variants:".to_string();
+                    let mut txt_to_send = "The word is found, here's a meaning list:\n".to_string();
                     vec_r.iter().for_each(|el| {
                         let mut temp_str = el.to_owned();
-                        temp_str.push_str("\\n");
+                        temp_str.push_str("\n");
                         txt_to_send.push_str(&temp_str)
                     });
 
@@ -77,5 +76,19 @@ async fn usual_text_handler(bot: teloxide::Bot, msg: Message) -> ResponseResult<
             }
         }
     }
+
     Ok(())
+}
+
+async fn search_dictionary_wrapper(
+    word: &str,
+) -> Result<ParseLinkResult, Box<dyn std::error::Error + Send + Sync>> {
+    let word = word.to_string();
+    tokio::task::spawn_blocking(move || {
+        // This code can use non-Send types safely!
+        let r = futures::executor::block_on(search_dictionary(&word));
+        r
+    })
+    .await
+    .unwrap()
 }
